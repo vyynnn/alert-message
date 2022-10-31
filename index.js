f610a0563ba2a8e3a88d702260f9e7f6140133a6
vyynnn <pogireyvine@gmail.com>
2022-10-31 14:29:36 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('.answer')
+const showAlert = document.querySelector('.showAlert')
+
+
+showAlert.onclick = function(){
+    answer()
+}
+
+function answer(){
+    let person = prompt('Enter your name')
+    if(person != null){
+        input.textContent = 'Hello ' + person + ' my Love so sweet and hello to my abiii also i love you both always, you know that i love chicken nuggets!!!'
+    }
+}
+
+alert ('hello always take care')
+alert('I always love you')
+alert('I always miss you')
+alert('d ako magsasawang paramdam na mahal kita')
+alert('sana lageng BEBETIME HAHAHA')
+
+// alert ('hello love i love you always oki?')
+
+
+
+
